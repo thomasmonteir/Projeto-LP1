af4b2e5234915d7d7cc7922496508f3abc688bc8
helio900 <105680383+helio900@users.noreply.github.com>
2022-05-17 14:46:01 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,48 +25,61 @@ int main()
     a[2] = new Gerente();
     a[3] = new Presidente();
 
-    cin >> codigo;
-    cin.ignore();
-    getline(cin,nome);
-    getline(cin,endereco);
-    getline(cin,tel);
-    getline(cin,ddi);
-    cin >> designacao;
-    cin >> salario;
-    cin.ignore();
+
+    while(1){
+        cout << "OPCAO 1: cadastrar novo usuario.\n"
+                "OPCAO 2: editar usuario.\n"
+                "OPCAO 3: sair\n";
+
+        int opcao;
+        cin>> opcao;
+
+        if(opcao == 1 || opcao == 2){
+            cin >> codigo;
+            cin.ignore();
+            getline(cin,nome);
+            getline(cin,endereco);
+            getline(cin,tel);
+            getline(cin,ddi);
+            cin >> designacao;
+            cin >> salario;
+            cin.ignore();
 
 
-    switch(designacao){
+            switch(designacao){
 
-        case 0:
+                case 0:
 
-            a[0]->setDados(codigo,nome,endereco,tel,ddi,designacao,salario);
-            a[0]->showDados();
+                    a[0]->setDados(codigo,nome,endereco,tel,ddi,designacao,salario);
+                    a[0]->showDados();
+                    break;
+
+
+                case 1:
+
+                    a[1]->setDados(codigo,nome,endereco,tel,ddi,designacao,salario);
+                    a[1]->showDados();
+                    break;
+
+
+                case 2:
+
+                    a[2]->setDados(codigo,nome,endereco,tel,ddi,designacao,salario);
+                    a[2]->showDados();
+                    break;
+
+
+                case 3:
+
+                    a[3]->setDados(codigo,nome,endereco,tel,ddi,designacao,salario);
+                    a[3]->showDados();
+                    break;
+
+            }
+        }else {
             break;
-
-
-        case 1:
-
-            a[1]->setDados(codigo,nome,endereco,tel,ddi,designacao,salario);
-            a[1]->showDados();
-            break;
-
-
-        case 2:
-
-            a[2]->setDados(codigo,nome,endereco,tel,ddi,designacao,salario);
-            a[2]->showDados();
-            break;
-
-
-        case 3:
-
-            a[3]->setDados(codigo,nome,endereco,tel,ddi,designacao,salario);
-            a[3]->showDados();
-            break;
-
+        }
     }
 
-
-
 }
+
