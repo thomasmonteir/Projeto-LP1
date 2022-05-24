62d0f815d421ea66ac6d47b45a34df4def3759e8
helio900 <105680383+helio900@users.noreply.github.com>
2022-05-24 17:03:05 -0300

    Update Diretor.cpp

--- Diretor.cpp
+++ Diretor.cpp
@@ -2,7 +2,7 @@
 
 Diretor::Diretor()
 {
-    //ctor
+    designacao = 1;
 }
 
 Diretor::~Diretor()
@@ -20,6 +20,7 @@ std::string Diretor::getAreaDeFormacao(){
     return areaDeFormacao;
 
 }
+
 void Diretor::setAreaDeSupervisao(std::string as){
 
     areaDeSupervisao = as;
@@ -30,5 +31,16 @@ void Diretor::setAreaDeFormacao(std::string a){
     areaDeFormacao= a;
 
 }
+void Diretor::getDadosAdd(){
 
+    getline(std::cin,areaDeFormacao);
+    getline(std::cin,areaDeSupervisao);
 
+}
+
+void Diretor::showDados(){
+
+     std::cout << codigo << " , " << nome << " , " << endereco << " , "<< tel << " , " << ddi << " , " << designacao << " , " << salario << " , " <<
+     areaDeFormacao <<  " , " << areaDeSupervisao << std::endl;
+
+}
