b36a15eecef1cdfa7e28ca09c74240425be78024
helio900 <105680383+helio900@users.noreply.github.com>
2022-05-24 17:04:20 -0300

    Update Gerente.h

--- Gerente.h
+++ Gerente.h
@@ -1,23 +1,119 @@
-#ifndef GERENTE_H
-#define GERENTE_H
-
 #include "Funcionario.h"
 
-#include <string>
-
-class Gerente: public Funcionario
+Funcionario::Funcionario()
 {
-    public:
-        Gerente();
-        virtual ~Gerente();
-        std::string getAreaDeSupervisao();
-        void setAreaDeSupervisao(std::string as);
+    //ctor
+}
 
-    protected:
+Funcionario::~Funcionario()
+{
+    //dtor
+}
 
-    private:
 
-        std::string areaDeSupervisao;
-};
+int Funcionario::getCodigo(){
 
-#endif // GERENTE_H
+    return codigo;
+
+}
+
+std::string Funcionario::getNome(){
+
+    return nome;
+
+}
+std::string Funcionario::getEndereco(){
+
+    return endereco;
+
+}
+std::string Funcionario::getTel(){
+
+    return tel;
+
+}
+std::string Funcionario::getDdi(){
+
+    return ddi;
+
+}
+int Funcionario::getDesignacao(){
+
+    return designacao;
+
+}
+float Funcionario::getSalario(){
+
+    return salario;
+
+}
+void Funcionario::setCodigo(int cod){
+
+    codigo = cod;
+
+}
+void Funcionario::setNome(std::string n){
+
+    nome = n;
+
+}
+void Funcionario::setEndereco(std::string e){
+
+    endereco = e;
+
+}
+void Funcionario::setTel(std::string tel){
+
+    this->tel = tel;
+
+}
+
+void Funcionario::setDdi(std::string d){
+
+    ddi = d;
+
+}
+void Funcionario::setDesignacao(int des){
+
+    designacao = des;
+
+}
+void Funcionario::setSalario(float s){
+
+    salario = s;
+
+}
+
+void Funcionario::setDados(int codigo,std::string nome,std::string endereco,std::string tel,std::string ddi,int designacao,float salario)
+{
+
+    this->codigo = codigo;
+    this->nome = nome;
+    this->endereco = endereco;
+    this->tel = tel;
+    this->ddi = ddi;
+    this->designacao = designacao;
+    this->salario = salario;
+
+
+}
+
+void Funcionario::showDados()
+{
+
+    std::cout << codigo << " , " << nome << " , " << endereco << " , "<< tel << " , " << ddi << " , " << designacao << " , " << salario << std::endl;
+
+}
+
+
+void Funcionario::getDadosAdd(){
+
+
+
+}
+void Funcionario::setAreaDeFormacao(std::string a){
+}
+void Funcionario::setAreaDeSupervisao(std::string as){
+}
+void Funcionario::setFormacaoAcademica(std::string f){
+}
