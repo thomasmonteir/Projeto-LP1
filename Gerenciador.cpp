a11aa2a31b121297688d1bd6ab07db775d279d01
helio900 <105680383+helio900@users.noreply.github.com>
2022-05-25 10:51:44 -0300

    Update Gerenciador.cpp

--- Gerenciador.cpp
+++ Gerenciador.cpp
@@ -1,4 +1,7 @@
 #include "Gerenciador.h"
+#include <iostream>
+
+using namespace std;
 
 Gerenciador::Gerenciador()
 {
@@ -15,42 +18,194 @@ void Gerenciador::setFuncionario(Funcionario *f){
 
 }
 
-void Gerenciador::alteraOperador(int codigo){
+void Gerenciador::alteraFuncionario(int codigo){
     int a, cod;
-    string nome, ddi, telefone, endereco;
+    std::string nome, ddi, telefone, endereco, areaDeFormacao, formacaoAcademica, areaDeSupervisao;
     float salario;
+    int tipo;
+
+
 
     for(int i = 0; i < funcionarios.size(); i++){
-        if(codigo == funcionarios[i].getCodigo){
-            cout << "Qual atributo você deseja alterar?\n\n1- Código\n2- Nome\n3- Endereço\n4- Telefone\n5- DDI\n6- Salário" << endl;
+        if(codigo == funcionarios[i]->getCodigo()){
+            tipo = funcionarios[i]->getDesignacao();
 
-            switch(a){
-                case 1:
-                    cin >> cod;
-                    funcionarios[i].setCodigo(cod);
+            switch(tipo){
+                case 0:
+                    cout << "Qual atributo você deseja alterar?\n\n1- Código\n2- Nome\n3- Endereço\n4- Telefone\n5- DDI\n6- Salário" << endl;
+                    cin >> a;
+
+                    switch(a){
+                        case 1:
+                            cin >> cod;
+                            funcionarios[i]->setCodigo(cod);
+                            break;
+                        case 2:
+                            cin.ignore();
+                            getline(std::cin, nome);
+                            funcionarios[i]->setNome(nome);
+                            break;
+                        case 3:
+                            cin.ignore();
+                            getline(std::cin, endereco);
+                            funcionarios[i]->setEndereco(endereco);
+                            break;
+                        case 4:
+                            cin.ignore();
+                            getline(std::cin, telefone);
+                            funcionarios[i]->setTel(telefone);
+                            break;
+                        case 5:
+                            cin.ignore();
+                            getline(std::cin, ddi);
+                            funcionarios[i]->setDdi(ddi);
+                            break;
+                        case 6:
+                            cin >> salario;
+                            funcionarios[i]->setSalario(salario);
+                            break;
+
+                    }
                     break;
-                case 2:
-                    getline(std::cin, nome);
-                    funcionarios[i].setNome(nome);
-                    break;
-                case 3:
-                    getline(std::cin, endereco);
-                    funcionarios[i].setEndereco(endereco);
-                    break;
-                case 4:
-                    getline(std::cin, telefone);
-                    funcionarios[i].setTelefone(telefone);
-                    break;
-                case 5:
-                    getline(std::cin, ddi);
-                    funcionarios[i].setDdi(ddi);
-                    break;
-                case 6:
-                    cin >> salario;
-                    funcionarios[i].setSalario(salario);
-                    break;
+
+                    case 1:
+                        cout << "Qual atributo você deseja alterar?\n\n 1- Código\n 2- Nome\n 3- Endereço\n 4- Telefone\n 5- DDI\n 6- Salário\n7- Área de supervisao\n8- Área de formação\n" << endl;
+                        cin >> a;
+
+                        switch(a){
+                            case 1:
+                                cin >> cod;
+                                funcionarios[i]->setCodigo(cod);
+                                break;
+                            case 2:
+                                cin.ignore();
+                                getline(std::cin, nome);
+                                funcionarios[i]->setNome(nome);
+                                break;
+                            case 3:
+                                cin.ignore();
+                                getline(std::cin, endereco);
+                                funcionarios[i]->setEndereco(endereco);
+                                break;
+                            case 4:
+                                cin.ignore();
+                                getline(std::cin, telefone);
+                                funcionarios[i]->setTel(telefone);
+                                break;
+                            case 5:
+                                cin.ignore();
+                                getline(std::cin, ddi);
+                                funcionarios[i]->setDdi(ddi);
+                                break;
+                            case 6:
+                                cin >> salario;
+                                funcionarios[i]->setSalario(salario);
+                                break;
+                            case 7:
+                                cin.ignore();
+                                getline(std::cin, areaDeSupervisao);
+                                funcionarios[i]->setAreaDeSupervisao(areaDeSupervisao);
+                                break;
+                            case 8:
+                                cin.ignore();
+                                getline(std::cin, areaDeFormacao);
+                                funcionarios[i]->setAreaDeFormacao(areaDeFormacao);
+                                break;
+
+                        }
+                        break;
+
+                        case 2:
+                            cout << "Qual atributo você deseja alterar?\n\n1- Código\n2- Nome\n3- Endereço\n4- Telefone\n5- DDI\n6- Salário\n7- Área de supervisão\n" << endl;
+                            cin >> a;
+
+                            switch(a){
+                                case 1:
+                                    cin >> cod;
+                                    funcionarios[i]->setCodigo(cod);
+                                    break;
+                                case 2:
+                                    cin.ignore();
+                                    getline(std::cin, nome);
+                                    funcionarios[i]->setNome(nome);
+                                    break;
+                                case 3:
+                                    cin.ignore();
+                                    getline(std::cin, endereco);
+                                    funcionarios[i]->setEndereco(endereco);
+                                    break;
+                                case 4:
+                                    cin.ignore();
+                                    getline(std::cin, telefone);
+                                    funcionarios[i]->setTel(telefone);
+                                    break;
+                                case 5:
+                                    cin.ignore();
+                                    getline(std::cin, ddi);
+                                    funcionarios[i]->setDdi(ddi);
+                                    break;
+                                case 6:
+                                    cin >> salario;
+                                    funcionarios[i]->setSalario(salario);
+                                    break;
+                                case 7:
+                                    cin.ignore();
+                                    getline(std::cin, areaDeSupervisao);
+                                    funcionarios[i]->setAreaDeSupervisao(areaDeSupervisao);
+                                    break;
+
+                            }
+                        break;
+
+                        case 3:
+                            cout << "Qual atributo você deseja alterar?\n\n1- Código\n2- Nome\n3- Endereço\n4- Telefone\n5- DDI\n6- Salário\n7- Área de fromação\n8- Formação Acadêmica\n" << endl;
+                            cin >> a;
+
+                            switch(a){
+                                case 1:
+                                    cin >> cod;
+                                    funcionarios[i]->setCodigo(cod);
+                                    break;
+                                case 2:
+                                    cin.ignore();
+                                    getline(std::cin, nome);
+                                    funcionarios[i]->setNome(nome);
+                                    break;
+                                case 3:
+                                    cin.ignore();
+                                    getline(std::cin, endereco);
+                                    funcionarios[i]->setEndereco(endereco);
+                                    break;
+                                case 4:
+                                    cin.ignore();
+                                    getline(std::cin, telefone);
+                                    funcionarios[i]->setTel(telefone);
+                                    break;
+                                case 5:
+                                    cin.ignore();
+                                    getline(std::cin, ddi);
+                                    funcionarios[i]->setDdi(ddi);
+                                    break;
+                                case 6:
+                                    cin >> salario;
+                                    funcionarios[i]->setSalario(salario);
+                                    break;
+                                case 7:
+                                    cin.ignore();
+                                    getline(std::cin, areaDeFormacao);
+                                    funcionarios[i]->setAreaDeFormacao(areaDeFormacao);
+                                    break;
+                                case 8:
+                                    cin.ignore();
+                                    getline(std::cin, formacaoAcademica);
+                                    funcionarios[i]->setFormacaoAcademica(formacaoAcademica);
+                                    break;
+
+                            }
+                            break;
 
             }
+
         }
     }
 }
