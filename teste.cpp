1b9d725372e4c6072e74fd35c5a09a129f10724a
Thomas Monteiro <79983346+thomasmonteir@users.noreply.github.com>
2022-06-07 15:18:22 -0300

    Add files via upload

---
+++ teste.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    time_t t;
+    size_t tam;
+    clock_t c;
+    struct tm * infoTempo;
+    int a;
+    
+    char buffer[3];
+    
+    time(&t);
+    infoTempo = localtime(&t);
+    
+   	strftime(buffer,3,"%m",infoTempo);
+   	
+	
+    cout << buffer;
+    
+    return 0;
+}
