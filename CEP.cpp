4c2ac368c5a9ff37a5f482467a4a3f083277a86f
Hélio II <105680383+helio900@users.noreply.github.com>
2022-06-22 15:41:00 -0300

    Add files via upload

---
+++ CEP.cpp
@@ -0,0 +1,58 @@
+#include "CEP.h"
+
+CEP::CEP()
+{
+    //ctor
+}
+
+std::string CEP::parseCep()
+{
+    fstream fs;
+    string linha,dado,rua,bairro,cidade,uf;
+    int i = 0,posPt = 0;
+
+    fs.open("cep.txt",fstream::in);
+
+    if(!fs.is_open()){
+
+        cout << "erro ao abrir arquivo\n";
+        return NULL;
+
+    }
+
+    while(!fs.eof()){
+
+        getline(fs,linha);
+
+        if(i == 2 || i == 4 || i ==5 || i ==6){
+            posPt = linha.find(':');
+            dado = linha.substr(posPt + 3,linha.size() - 2 - posPt - 3);
+            //dado = dado.substr(0,dado.size() - 2);
+            //cout << dado << endl;
+
+            if(i == 2){
+
+                rua = dado;
+            }
+            else if(i == 4){
+
+                bairro = dado;
+
+            }
+            else if( i == 5){
+
+                cidade = dado;
+
+            }
+            else if(i == 6){
+
+                uf = dado;
+
+            }
+        }
+        i++;
+    }
+
+    return rua + ", " + bairro + ". " + cidade + ", " + uf;
+
+}
---
+++ CEP.h
@@ -0,0 +1,16 @@
+#ifndef CEP_H
+#define CEP_H
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class CEP
+{
+    public:
+        CEP();
+        std::string parseCep();
+};
+
+#endif // CEP_H
